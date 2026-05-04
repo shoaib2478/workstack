913938c3c1176d3936e2de731c2232373c52f964
shuaib.sayyad <shoaib2478@gmail.com>
2026-05-04 19:21:02 +0530

    Role permission join table added

--- backend/apps/rbac/models.py
+++ backend/apps/rbac/models.py
@@ -2,6 +2,7 @@ from enum import unique
 from re import L
 from django.db import models
 from core.models import BaseTimeStampedModel
+from apps.users.models import User
 
 class Permission(BaseTimeStampedModel):
     code = models.CharField(max_length = 128, unique = True, help_text = "'payroll:read', 'employee:write', 'org_chart:delete'")
@@ -15,9 +16,7 @@ class Role(BaseTimeStampedModel):
     )
     name = models.CharField(max_length=100, help_text = "Acme Corp can have a custom SuperManager role")
     description = models.TextField(blank=True)
-    
-    # [Database Design] ManyToManyField automatically creates the join table for us.
-    permissions = models.ManyToManyField(Permission, related_name='roles', blank=True)
+    last_modified_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="modified_roles")
 
     class Meta:
         # Prevent duplicate role names within the same company
@@ -27,6 +26,15 @@ class Role(BaseTimeStampedModel):
 
     def __str__(self):
         return f"{self.name} ({self.organization.name})"
+
+class RolePermission(BaseTimeStampedModel):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    
+    granted_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="granted_rolepermissions")
+    last_modified_by = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="modified_rolepermissions"
+    )
 
 class MemberRole(BaseTimeStampedModel):
     member = models.ForeignKey(
@@ -40,6 +48,20 @@ class MemberRole(BaseTimeStampedModel):
         related_name='member_assignments'
     )
 
+    granted_by = models.ForeignKey(
+        User, 
+        null=True, 
+        on_delete=models.SET_NULL, 
+        related_name="granted_memberroles"
+    )
+
+    last_modified_by = models.ForeignKey(
+        User, 
+        null=True, 
+        on_delete=models.SET_NULL, 
+        related_name="modified_memberroles"
+    )
+    
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['member', 'role'], name='unique_member_role')
